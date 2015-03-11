90dfc7c109db65f6319cfed935cdb1c62f8265ae
=makoto <=makoto@cdn.pub>
2015-03-11 08:56:52 +0000

    [add] jquery

--- jquery.sh
+++ jquery.sh
@@ -4,4 +4,7 @@ set -e
 [ "$1" = ""      ] && echo "$0 version"       && exit 1
 [ -e "jquery/$1" ] && echo "exist: jquery/$1" && exit 1
 cd `dirname $0`
-git clone --depth 1 -b $1 git://github.com/jquery/jquery.git jquery/$1
+git submodule add git@github.com:jquery/jquery.git jquery/$1
+cd jquery/$1
+git checkout $1
+
