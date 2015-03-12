ef7570fb42a72b557db11aeb87b5e127379ba819
=makoto <=makoto@cdn.pub>
2015-03-12 16:12:53 +0000

    [add] angular-strap

---
+++ angular-strap/sync.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+cd `dirname $0`
+######################################################################
+exec aws s3 sync ./ s3://cdn.pub/angular-strap/ \
+--region ap-northeast-1 \
+--size-only \
+--cache-control "max-age=315360000" \
+--exclude '*' \
+--include '*/dist/*' \
+
