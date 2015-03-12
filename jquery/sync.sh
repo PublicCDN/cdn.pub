7d3009734c555c461c9676923b6d2f10ddc26ffb
=makoto <=makoto@cdn.pub>
2015-03-12 13:51:44 +0000

    [add] jquery/sync.sh

---
+++ jquery/sync.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+cd `dirname $0`
+######################################################################
+# jquery
+aws s3 sync ./ s3://cdn.pub/jquery/ \
+--region ap-northeast-1 \
+--size-only \
+--cache-control "max-age=31536000" \
+--exclude '*' \
+--include '*/dist/*' \
+--exclude '*/src/*'
+
--- sync.sh
+++ sync.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 set -e
 cd `dirname $0`
-exec aws s3 sync ./ s3://cdn.pub/jquery/ \
---region ap-northeast-1 \
---dryrun \
---size-only \
---cache-control "max-age=31536000" \
---exclude '*/src/*' \
---exclude '*/test/*' \
---exclude '*/tests/*' \
---exclude '*/build/*' \
---exclude '*/external/*' \
---exclude '*/.git/*' \
---exclude 'README.md' \
---exclude '*/.*' \
---exclude '.git/*' \
---exclude '.*' 
-
-
+jquery/sync.sh
