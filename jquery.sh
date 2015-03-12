604202a634ad127dcd507ad2d7bf0d6dd53f3ea2
=makoto <=makoto@cdn.pub>
2015-03-12 14:04:39 +0000

    [modify] jquery.sh

--- jquery.sh
+++ jquery.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
-
-[ "$1" = ""      ] && echo "$0 version"       && exit 1
-[ -e "jquery/$1" ] && echo "exist: jquery/$1" && exit 1
+name='jquery'
+repo='git@github.com:jquery/jquery.git'
+[ "$1" = ""     ] && echo "$0 version"      && exit 1
+[ -e "$name/$1" ] && echo "exist: $name/$1" && exit 1
 cd `dirname $0`
-git submodule add git@github.com:jquery/jquery.git jquery/$1
-cd jquery/$1
+git submodule add $repo $name/$1
+cd $name/$1
 git checkout $1
 
