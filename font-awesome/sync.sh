8a8212a3c21c49a19d2a2f1a0849fceffa636baa
=makoto <=makoto@cdn.pub>
2015-03-12 14:40:22 +0000

    [add] bootswatch

--- font-awesome/sync.sh
+++ font-awesome/sync.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 aws s3 sync ./ s3://cdn.pub/bootstrap/ \
 --region ap-northeast-1 \
 --size-only \
---cache-control "max-age=31536000" \
+--cache-control "max-age=315360000" \
 --exclude '*' \
 --include '*/css/*' \
 --include '*/fonts/*' \
--- jquery/sync.sh
+++ jquery/sync.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 exec aws s3 sync ./ s3://cdn.pub/jquery/ \
 --region ap-northeast-1 \
 --size-only \
---cache-control "max-age=31536000" \
+--cache-control "max-age=315360000" \
 --exclude '*' \
 --include '*/dist/*' \
 --exclude '*/src/*'
