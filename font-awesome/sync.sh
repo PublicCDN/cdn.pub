fdb2ad04d617e55955f36ff210bb395c4a56529c
=makoto <=makoto@cdn.pub>
2015-03-12 14:18:05 +0000

    色々

---
+++ font-awesome/sync.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+cd `dirname $0`
+######################################################################
+aws s3 sync ./ s3://cdn.pub/bootstrap/ \
+--region ap-northeast-1 \
+--size-only \
+--cache-control "max-age=31536000" \
+--exclude '*' \
+--include '*/css/*' \
+--include '*/fonts/*' \
+--exclude '*/src/*'
