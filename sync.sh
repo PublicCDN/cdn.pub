92e21a2734fc9b90cfed47573af08b3b527c854c
=makoto <=makoto@cdn.pub>
2015-03-16 05:33:33 +0000

    modified:   sync.sh

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-set -e
 cd `dirname $0`
 jquery/sync.sh
 bootstrap/sync.sh
 Font-Awesome/sync.sh
 angular/sync.sh
-
+angular-strap/sync.sh
+bootswatch/sync.sh
+requirejs/sync.sh
+html5shiv/sync.sh
